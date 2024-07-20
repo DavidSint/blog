da57d02c231c2563f6c9198e45aa5738120ae4bc
David Sint <david@sint.dev>
2024-07-20 23:22:33 +0100

    add service-worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
+import serviceWorker from "astrojs-service-worker";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.davidsint.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), serviceWorker()],
 });
